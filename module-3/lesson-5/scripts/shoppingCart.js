cdd6252bbd56f5bd89063d0adbba66ba9196a1ee
phillip barlow <philb60@hotmail.co.uk>
2021-02-25 21:08:28 +0000

    3.5.2 + 3.5.3a complete
    3.5.2 + 3.5.3a complete

--- module-3/lesson-5/scripts/shoppingCart.js
+++ module-3/lesson-5/scripts/shoppingCart.js
@@ -61,22 +61,41 @@ let shoppingCart = [
 }];
 // console.log(shoppingCart[0].name)
 
-function getTotalPrice(shoppingCart){
+function getTotalPrice(shoppingCart, objCoupon={}){
+  //setting total of cart to 0 before loop starts
   let total = 0;
+  //setting the item price to 0 so it isnt declared in the loop
   let itemPrice = 0;
-  
+  //loops through each item in the shopping cart array
   for(let counter = 0; counter < shoppingCart.length; counter++){
+    //select the current cart item using the counter as the array key
     let objItem = shoppingCart[counter];
+    //multiply the cost of the item by the quantity of the item
     itemPrice = parseFloat(objItem.quantity) * parseFloat(objItem.price);
+    //if a coupon has been passed as an argument AND the coupon type is percentage
+    if(objCoupon && objCoupon.type === 'percentatge'){
+      //if the coupon category =the current item type OR coupon category = all
+      if(objCoupon.category == objItem.type || objCoupon.category == 'all'){
+        //itemPrice = percentage decrease for all item
+        itemPrice = (itemPrice / 100) * (100 - objCoupon.value);
+      }
+    }
+    //total = old total + current item price
     total = total + itemPrice;
   }
+  //if coupon has been passed as an argument AND the coupon type is total
+  if(objCoupon && objCoupon.type == 'total'){
+    //decrease the total by the value of the coupon
+    total = total - objCoupon.value;
+  }
+  //return the total price of the cart(with dsicount applied where applicable)
   return total.toFixed(2)
 }
 let objCoupon = {
   name:'20OFF',
   value:20,
   category:'food',
-  type:'percentage'
+  type:'total'
 }
-let cartTotal = getTotalPrice(shoppingCart);
+let cartTotal = getTotalPrice(shoppingCart, objCoupon);
 console.log(cartTotal)
