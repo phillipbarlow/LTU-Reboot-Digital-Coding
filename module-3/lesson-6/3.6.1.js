12681613bddd28b2ab4d1eca1c968e86e6c1668d
phillip barlow <philb60@hotmail.co.uk>
2021-03-02 19:41:10 +0000

    3.6.1 complete
    3.6.1 complete

---
+++ module-3/lesson-6/3.6.1.js
@@ -0,0 +1,6 @@
+let element = document.querySelector('.selectMe');
+console.log(element)
+
+element.setAttribute('phil', 'example');
+let h1 = document.querySelector("h1");
+h1.style.fontSize='100px';
